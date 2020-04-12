6fab6bf5caf01fcfe1cb3763dced69a8b88f1ad6
Rodrigolmg <rodrigo.leitemg@gmail.com>
2020-04-12 10:19:39 -0300

    REST testado

--- src/main/java/com/curso/exemplo/resources/CategoriaResource.java
+++ src/main/java/com/curso/exemplo/resources/CategoriaResource.java
@@ -1,16 +1,59 @@
 package com.curso.exemplo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.exemplo.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-
+	
+	@GetMapping(value = "/{id}")
+	public Categoria consultar(@PathVariable("id") Integer id) {
+		
+		Categoria cat1 = new Categoria();
+		Categoria cat2 = new Categoria();
+		
+		cat1.setId(1);
+		cat2.setId(2);
+		
+		cat1.setNome("Informática");
+		cat2.setNome("Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias.get(id - 1);
+	}
+	
 	@GetMapping
-	public String listar() {
-		return "REST testado e funcinoando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria();
+		Categoria cat2 = new Categoria();
+		
+		cat1.setId(1);
+		cat2.setId(2);
+		
+		cat1.setNome("Informática");
+		cat2.setNome("Escritório");
+
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 }
